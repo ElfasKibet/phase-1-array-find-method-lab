df6b8778acedfae65b849302259ff3d90af02dfa
elfas <kibetelfas17@gmail.com>
2025-03-20 21:22:07 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+// Function to find the first Super Bowl win
+function superbowlWin(record) {
+  const win = record.find((game) => game.result === "W");
+  return win ? win.year : undefined;
+}
+
+// Export the function for testing
+module.exports = superbowlWin;
